b9e29087612d6dbafbbe0680f52722617fd69885
Valentin Bourdaud <bourdaudv@gmail.com>
2018-05-22 17:04:15 +0200

    Initialisation de la JSP

--- src/main/java/dev/sgp/web/EditerCollaborateurControlleur.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurControlleur.java
@@ -10,55 +10,49 @@ import javax.servlet.http.HttpServletResponse;
 public class EditerCollaborateurControlleur extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
-	ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		String matriculeParam = req.getParameter("matricule");
-		
-		if(matriculeParam==null){
+
+		if (matriculeParam == null) {
 			resp.setStatus(400);
 		}
 		resp.setContentType("text/html");
-		
-		resp.getWriter().write("<h1>Edition de collaborateur</h1>"
-				+ "<p>Matricule :</p> " 
-				+ matriculeParam);
-			}
+
+		resp.getWriter().write("<h1>Edition de collaborateur</h1>" + "<p>Matricule :</p> " + matriculeParam);
+	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws
-	ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		String matriculeParam = req.getParameter("matricule");
-		if(matriculeParam==null){
+		if (matriculeParam == null) {
 			resp.setStatus(400);
 			resp.getWriter().write("Un matricule était attendu");
 
 		}
 		String titreParam = req.getParameter("titre");
-		if(titreParam==null){
+		if (titreParam == null) {
 			resp.setStatus(400);
 			resp.getWriter().write("Un titre était attendu");
 		}
 		String nomParam = req.getParameter("nom");
-		if(nomParam==null){
+		if (nomParam == null) {
 			resp.setStatus(400);
 			resp.getWriter().write("Un nom était attendu");
-			
+
 		}
 		String prenomParam = req.getParameter("prenom");
-		if(prenomParam==null){
+		if (prenomParam == null) {
 			resp.setStatus(400);
 			resp.getWriter().write("Un prénom était attendu");
 		}
-		
 
-		
 		resp.setContentType("text/html");
-		
-		resp.getWriter().write("<h1>Creation d'un collaborateur avec les informations suivantes :</h1>"
-				+ "<ul>"
-				+ "<li>matricule="+ matriculeParam +",titre="+titreParam+",nom="+nomParam+",prenom="+prenomParam+"</li>"
-				+ "</ul>");
-			}
+
+		resp.getWriter()
+				.write("<h1>Creation d'un collaborateur avec les informations suivantes :</h1>" + "<ul>"
+						+ "<li>matricule=" + matriculeParam + ",titre=" + titreParam + ",nom=" + nomParam + ",prenom="
+						+ prenomParam + "</li>" + "</ul>");
+	}
 }
--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,21 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ListerCollaborateursController extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
-	ServletException, IOException {
-		resp.getWriter().write("Coucou ListerCollaborateursController");
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String avecPhotoParam = req.getParameter("avecPhoto");
-		
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
-		
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-				+ "<ul>"
-				+ "<li>avecPhoto="+ avecPhotoParam+ "</li>"
-				+ "<li>departement="+ departementParam + "</li>"
-				+ "</ul>");
-		
+		req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
+
 	}
-	
+
 }
